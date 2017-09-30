ec141831159c7059522072c1ec1e65871d41de69
leebj522 <leebj522@gmail.com>
2017-09-30 17:47:21 +0900

    Update GTest.java

--- src/gTest/GTest.java
+++ src/gTest/GTest.java
@@ -4,7 +4,7 @@ public class GTest {
 	public static void main(String[] args) {
 		System.out.println("Git Test");
 		System.out.println("11111111111");
-		System.out.println("테스트 테스트");
+		System.out.println("GitHub Test");
 	}
 }
  
